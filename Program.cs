5e517ec253da8d61932ef3bc434078e833aae375
AlbertoGruning-windows <kimono76.live@gmail.com>
2021-10-17 17:50:41 -0300

    cap39-fix

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AirlineSendAgent.App;
 
 namespace AirlineSendAgent
 {
@@ -8,12 +9,12 @@ namespace AirlineSendAgent
     {
         static void Main(string[] args)
         {
-            var host= Host.CreateDefaultHostBuilder()
+            var host= Host.CreateDefaultBuilder()
                 .ConfigureServices((context,services) =>
-                    services.AddSingleton<IntAppHost,AppHost>();
+                    services.AddSingleton<IntAppHost,AppHost>()
                 ).Build();
             
-            host.services.GetService<IntAppHost>().Run();
+            host.Services.GetService<IntAppHost>().Run();
         }
     }
 }
