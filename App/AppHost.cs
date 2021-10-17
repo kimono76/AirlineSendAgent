407703ebf1a2f362a3c0b197396456b84aab215b
AlbertoGruning-windows <kimono76.live@gmail.com>
2021-10-17 19:08:31 -0300

    cap40-rabbitMq event listener

--- App/AppHost.cs
+++ App/AppHost.cs
@@ -1,9 +1,35 @@
+using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
 namespace AirlineSendAgent.App
 {
     public class AppHost:IntAppHost
     {
         public void Run(){
-            
+            var factory = new ConnectionFactory(){ HostName="localhost", Port=5672 };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel()){
+                channel.ExchangeDeclare(exchange:"trigger", type: ExchangeType.Fanout);
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(
+                    queue: queueName,
+                    exchange:"trigger",
+                    routingKey:""
+                    );
+                var consumer = new EventingBasicConsumer(channel);
+                Console.WriteLine("Listening on the message bus ...");
+
+                consumer.Received += async (ModuleHandle, ea)=>{
+                    System.Console.WriteLine("Event is triggered!");
+                };
+                channel.BasicConsume(
+                    queue:queueName,
+                    autoAck: true,
+                    consumer:consumer
+                );
+                Console.ReadLine();
+            }
         }
     }
 }
