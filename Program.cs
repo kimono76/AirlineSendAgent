29eae202e7f3e1fd7507260351e00c6ebdbcdc63
AlbertoGruning-windows <kimono76.live@gmail.com>
2021-10-17 19:49:54 -0300

    cap41-config and context

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AirlineSendAgent.App;
+using AirlineSendAgent.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace AirlineSendAgent
 {
@@ -10,11 +13,13 @@ namespace AirlineSendAgent
         static void Main(string[] args)
         {
             var host= Host.CreateDefaultBuilder()
-                .ConfigureServices((context,services) =>
-                    services.AddSingleton<IntAppHost,AppHost>()
-                ).Build();
+                .ConfigureServices((context,services) =>{
+                    services.AddSingleton<IntAppHost,AppHost>();
+                    services.AddDbContext<SendAgentDbContext>(opt => opt.UseSqlServer(context.Configuration.GetConnectionString("AirlineConnection")));
+                }).Build();
             
             host.Services.GetService<IntAppHost>().Run();
+
         }
     }
 }
